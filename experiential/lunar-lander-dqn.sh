a6817d837957da1c6f5b527d0364eb688fd7afb9
Rohan Badlani <rohan.badlani@gmail.com>
2019-02-23 02:53:09 -0800

    DQN Wrapper for Lunar Lander.

---
+++ experiential/lunar-lander-dqn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash          
+
+ENV_NAME="LunarLander-v2"
+LOG_DIR="logs"
+MODEL_DIR="models"
+PLOT_DIR="plots"
+
+TRAIN_TIMESTEPS="100000"
+EVAL_TIMESTEPS="1000"
+
+RESULTS_DIR="LunarLander-results"
+
+python train_dqn.py "${ENV_NAME}" "${TRAIN_TIMESTEPS}" "${LOG_DIR}" "${MODEL_DIR}" "${PLOT_DIR}"
+
+python evaluate_dqn.py "${ENV_NAME}" "${TRAIN_TIMESTEPS}" "${EVAL_TIMESTEPS}" "${MODEL_DIR}" "${RESULTS_DIR}"
