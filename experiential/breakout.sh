01b6f102df69ce5d02e60074a2c76287e51c9dee
Rohan Badlani <rohan.badlani@gmail.com>
2019-02-23 02:55:57 -0800

    DQN Wrapper for Breakout (Curiosity works on BreakOut Atari).

---
+++ experiential/breakout.sh
@@ -0,0 +1,15 @@
+#!/bin/bash          
+
+ENV_NAME="Breakout-ram-v0"
+LOG_DIR="logs"
+MODEL_DIR="models"
+PLOT_DIR="plots"
+
+TRAIN_TIMESTEPS="100000"
+EVAL_TIMESTEPS="1000"
+
+RESULTS_DIR="BreakOut-results"
+
+python train_dqn.py "${ENV_NAME}" "${TRAIN_TIMESTEPS}" "${LOG_DIR}" "${MODEL_DIR}" "${PLOT_DIR}"
+
+python evaluate_dqn.py "${ENV_NAME}" "${TRAIN_TIMESTEPS}" "${EVAL_TIMESTEPS}" "${MODEL_DIR}" "${RESULTS_DIR}"
